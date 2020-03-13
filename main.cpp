bbd65e4b215268aea960d44e8da4f45fee844da3
VoronovaMasha <56344724+VoronovaMasha@users.noreply.github.com>
2020-03-13 14:34:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+/* подключение заголовочных файлов и классов*/
 #include "mainwindow.h"
 
 #include <QApplication>
@@ -7,24 +8,26 @@
 #include "nrtlmanager.h"
 #include "nrtlmodel.h"
 
+/*Вызов функцию loadTxtFile, которая принимает адрес объекта класса QString, и после исполнения должна вернуть объект класса QString*/
 QString loadTxtFile(const QString& path);
 
 
-int main(int argc, char *argv[])
-{
-    QApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
-    QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
-    QApplication a(argc, argv);
-    a.setApplicationName("Nerve Tracts Lab");
-    a.setOrganizationName("Higher School of Economics");
-    a.setOrganizationDomain("hse.ru");
-    QSurfaceFormat format;
-    format.setVersion(2,1);
-    format.setProfile(QSurfaceFormat::CoreProfile);
-    format.setDepthBufferSize(32);
-    format.setSamples(16);
+int main(int argc, char *argv[])//Аргумент argc типа integer содержит в себе количество аргументов командной строки.Аргумент argv типа char - указатель на массив строк
+
+{/*Задание формата версии данного виджета*/
+    QApplication::setAttribute(Qt::AA_UseDesktopOpenGL);//установка атрибута AA_UseDesktopOpenGL для объекта QApplication
+    QApplication::setAttribute(Qt::AA_ShareOpenGLContexts); //установка атрибута AA_ShareOpenGLContexts для объекта QApplication
+    QApplication a(argc, argv); //создает новый экземпляр типа QApplication и вызывает конструктор этого класса
+    a.setApplicationName("Nerve Tracts Lab");//Устанавливает уникальное имя этого приложения 
+    a.setOrganizationName("Higher School of Economics");//Устанавливает уникальное имя домена организации
+    a.setOrganizationDomain("hse.ru");//Устанавливает уникальное имя домена организации
+    QSurfaceFormat format; //Класс QSurfaceFormat представляет формат QSurface.
+    format.setVersion(2,1);//установка формата версии 2.1
+    format.setProfile(QSurfaceFormat::CoreProfile);//
+    format.setDepthBufferSize(32);//задание буфера  глубины
+    format.setSamples(16);// подключение буффера мультисэмпла
     QSurfaceFormat::setDefaultFormat(format);
-    NrtlModel* md = new NrtlModel();
+    NrtlModel* md = new NrtlModel();//создание новой модели
     NrtlManager::initModel(md);
     MainWindow w;
     w.setModel(md);
@@ -33,12 +36,13 @@ int main(int argc, char *argv[])
     return a.exec();
 }
 
+/*Вызов функцию loadTxtFile, которая принимает адрес объекта класса QString, и после исполнения должна вернуть объект класса QString*/
 QString loadTxtFile(const QString& path)
 {
-    QFile file(path);
-    file.open(QFile::ReadOnly);
-    QTextStream tin(&file);
-    QString result;
+    QFile file(path);//Класс QFile предоставляет интерфейс для чтения и записи в файлы.
+    file.open(QFile::ReadOnly);//открытие файла для чтения
+    QTextStream tin(&file);//интерфейс для чтения и записи
+    QString result;//предоставление строки символов Unicode
 
     while(!tin.atEnd())
     {
@@ -46,7 +50,8 @@ QString loadTxtFile(const QString& path)
  //       result += "\n";
     }
 
-    qDebug() << result;
-    return result;
+    qDebug() << result;//Класс QDebug предоставляет выходной поток для отладочной информации.
+
+    return result;// возвращает результат
 }
 
